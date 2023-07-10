06ecb2d8e2912b3f5a994f91968032fb49590431
Amanda <amanda@pop-os.localdomain>
2023-07-10 14:00:34 +0200

    added the frame of the to-do-list app

---
+++ src/main/java/AppFrame.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class AppFrame extends JFrame {
+
+    private TitleBar title;
+    private Footer footer;
+    private List list;
+
+    private JButton newTask;
+    private JButton clear;
+
+    AppFrame() {
+        this.setSize(400, 600); // 400 width and 600 height
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close on exit
+        this.setVisible(true); // Make visible
+
+        title = new TitleBar();
+        footer = new Footer();
+        list = new List();
+
+        this.add(title, BorderLayout.NORTH); // Add title bar on top of the screen
+        this.add(footer, BorderLayout.SOUTH); // Add footer on bottom of the screen
+        this.add(list, BorderLayout.CENTER); // Add list in middle of footer and title
+
+        newTask = footer.getNewTask();
+        clear = footer.getClear();
+
+        addListeners();
+    }
+
+    public void addListeners() {
+        newTask.addMouseListener(new MouseAdapter() {
+            @override
+            public void mousePressed(MouseEvent e) {
+                Task task = new Task();
+                list.add(task); // Add new task to list
+                list.updateNumbers(); // Updates the numbers of the tasks
+
+                task.getDone().addMouseListener(new MouseAdapter() {
+                    @override
+                    public void mousePressed(MouseEvent e) {
+
+                        task.changeState(); // Change color of task
+                        list.updateNumbers(); // Updates the numbers of the tasks
+                        revalidate(); // Updates the frame
+
+                    }
+                });
+            }
+
+        });
+
+        clear.addMouseListener(new MouseAdapter() {
+            @override
+            public void mousePressed(MouseEvent e) {
+                list.removeCompletedTasks(); // Removes all tasks that are done
+                repaint(); // Repaints the list
+            }
+        });
+    }
+
+}
