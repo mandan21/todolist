80f90bfe5cc0d2e6e21cf40ae10ba297dd8e5ab9
Amanda <amanda@pop-os.localdomain>
2023-07-10 14:03:05 +0200

    the main class to run the application

---
+++ src/main/java/ToDoList.java
@@ -0,0 +1,28 @@
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Component;
+
+public class ToDoList {
+
+    public static void main(String args[]) {
+        AppFrame frame = new AppFrame(); // Create the frame
+    }
+}
+
+@interface override {
+
+}
